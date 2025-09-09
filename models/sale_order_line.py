2e0f2bf037c03cecd7d31089233f90e6a8651aba
Shamim Razu <shamimhossenrazu@gmail.com>
2025-09-09 20:52:39 +0600

    product quantity cacl from set name and vice varsa

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -5,25 +5,32 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     set_name = fields.Char(
-        string='Qty (dozen/pieces)',
+        string='Dozens / Pieces',
         compute='_compute_set_name',
+        inverse='_inverse_set_name',
         store=True,
         readonly=False,
         precompute=True)
 
-    @api.depends('product_packaging_qty', 'product_packaging_id')
+
+    @api.depends('product_uom_qty')
     def _compute_set_name(self):
         for rec in self:
-            if not rec.product_packaging_qty:
+            if not rec.product_uom_qty:
                 rec.set_name = ''
                 continue
+            dozen = int(rec.product_uom_qty // 12)
+            pieces = int(rec.product_uom_qty % 12)
+            rec.set_name = f"{dozen} / {pieces}"
 
-            whole_part = int(rec.product_packaging_qty)
-            decimal_part = rec.product_packaging_qty - whole_part
 
-            if decimal_part == 0:
-                rec.set_name = str(whole_part)
-            else:
-                # Convert decimal to pieces (multiply by 12)
-                pieces = int(round(decimal_part * 12))
-                rec.set_name = f"{whole_part} / {pieces}"
+    def _inverse_set_name(self):
+        for rec in self:
+            if not rec.set_name:
+                rec.product_uom_qty = 0
+                continue
+            try:
+                dozen, pieces = map(int, rec.set_name.split('/'))
+                rec.product_uom_qty = (dozen * 12) + pieces
+            except ValueError:
+                rec.product_uom_qty = 0
